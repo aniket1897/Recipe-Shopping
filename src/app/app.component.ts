552c3de80ae7e433b57f8776e0da12f3f8a38190
aniket1897 <paianiket4@gmail.com>
2018-10-06 16:30:11 +0530

    Added routing to navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   nameToBeAdded:string;
 
+   evaluate(name:string){
+      this.nameToBeAdded = name;
+      console.log(this.nameToBeAdded);
+   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from "@angular/core";
-
+import { Component, EventEmitter, Output,Input } from "@angular/core";
 @Component({
   selector:'app-header',
   templateUrl:'./header.component.html',
   styleUrls:['./header.component.css']
 })
 export class HeaderComponent{
+  @Output()
+  urlClicked = new EventEmitter<string>();
 
+  recipesLinkClicked(){
+    this.urlClicked.emit('Recipe');
+  }
+  
+  shoppingLinkClicked(){
+    console.log('clicked shopping');
+    this.urlClicked.emit('Shopping');
+  }
 }
